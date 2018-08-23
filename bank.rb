077135e549bec2fb6dafecf154cf688f08de0995
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-08-23 20:46:29 +1000

    add balance, deposit, withdraw

--- bank.rb
+++ bank.rb
@@ -1,3 +1,46 @@
+# Davo's banking app with basic functionality
+
+# assign global variables
+$balance = 0.0
+$message = ""
+
+def main
+  system('clear')
+  displayMenu
+  if $message != ""
+    puts $message
+    puts "-----------------------------"
+  end
+  $message = ""
+  print "Choose Option (1-4): "
+  input = gets.chomp
+  case input
+  when "1"
+    $message = "Balance: $%.2f" % $balance
+    main
+  when "2"
+    print "How much to deposit: $"
+    amount = getAmount
+    updateBalance(amount)
+    $message = "Succesfully Deposited $%.2f" % amount
+    $message += "\nNew Balance: $%.2f" % $balance
+    main
+  when "3"
+    print "How much to withdraw: $"
+    amount = getAmount
+    updateBalance(-amount)
+    $message = "Succesfully Withdrew $%.2f" % amount
+    $message += "\nNew Balance: $%.2f" % $balance
+    main
+  when "4"
+    return
+  else
+    $message = "ERROR: Wrong input entered"
+    main
+  end
+end
+
+# prints menu to screen using ASCII art :P
 def displayMenu
   puts "-----------------------------"
   puts "|     Davo's Banking App    |"
@@ -11,4 +54,23 @@ def displayMenu
   puts "-----------------------------"
 end
 
-displayMenu
+def getAmount
+  amount = gets.chomp.to_f
+  if amount < 0
+    system('clear')
+    $message = "ERROR: Must be positive number"
+  end
+  amount
+end
+
+def updateBalance(amount)
+  # check if withdrawing and there is enough funds
+  if amount < 0 and ($balance - amount) < 0
+    system('clear')
+    $message = "ERROR: Not enough funds"
+  end
+  $balance += amount.round(2)
+end
+
+# run program
+main
