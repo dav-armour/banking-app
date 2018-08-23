909ab4540326f93d7b1c4492a72038a73c7592e0
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-08-23 21:13:14 +1000

    add basic login menu

--- bank.rb
+++ bank.rb
@@ -4,7 +4,18 @@
 $balance = 0.0
 $message = ""
 
-def main
+def loginMenu
+  # FIXME: implement login
+  displayLogin
+  print "Username: "
+  user = gets.chomp
+  print "Password: "
+  pass = gets.chomp
+  $message = "Welcome #{user.capitalize}"
+  mainMenu
+end
+
+def mainMenu
   system('clear')
   displayMenu
   if $message != ""
@@ -17,30 +28,30 @@ def main
   case input
   when "1"
     $message = "Balance: $%.2f" % $balance
-    main
+    mainMenu
   when "2"
     print "How much to deposit: $"
     amount = getAmount
     updateBalance(amount)
     $message = "Succesfully Deposited $%.2f" % amount
     $message += "\nNew Balance: $%.2f" % $balance
-    main
+    mainMenu
   when "3"
     print "How much to withdraw: $"
     amount = getAmount
     updateBalance(-amount)
     $message = "Succesfully Withdrew $%.2f" % amount
     $message += "\nNew Balance: $%.2f" % $balance
-    main
+    mainMenu
   when "4"
     return
   else
-    $message = "ERROR: Wrong input entered"
-    main
+    $message = "ERROR: Invalid selection!"
+    mainMenu
   end
 end
 
-# prints menu to screen using ASCII art :P
+# prints main menu to screen using ASCII art :P
 def displayMenu
   puts "-----------------------------"
   puts "|     Davo's Banking App    |"
@@ -50,6 +61,19 @@ def displayMenu
   puts "| 2) Deposit Money          |"
   puts "| 3) Withdraw Money         |"
   puts "| 4) Exit                   |"
+  puts "|                           |"
+  puts "-----------------------------"
+end
+
+def displayLogin
+  puts "-----------------------------"
+  puts "|     Davo's Banking App    |"
+  puts "-----------------------------"
+  puts "|                           |"
+  puts "|                           |"
+  puts "|   Please login            |"
+  puts "|      to cointinue...      |"
+  puts "|                           |"
   puts "|                           |"
   puts "-----------------------------"
 end
@@ -73,4 +97,4 @@ def updateBalance(amount)
 end
 
 # run program
-main
+loginMenu()
