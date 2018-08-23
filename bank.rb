3730e0b623c15eeb76507df50e3f83d7db5ee1ff
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-08-23 19:51:36 +1000

    add menu display

---
+++ bank.rb
@@ -0,0 +1,14 @@
+def displayMenu
+  puts "-----------------------------"
+  puts "|     Davo's Banking App    |"
+  puts "-----------------------------"
+  puts "|                           |"
+  puts "| 1) View Balance           |"
+  puts "| 2) Deposit Money          |"
+  puts "| 3) Withdraw Money         |"
+  puts "| 4) Exit                   |"
+  puts "|                           |"
+  puts "-----------------------------"
+end
+
+displayMenu
